f074a7da06542d6226f069ec2609d021e5235a3f
michale <lxfeng1163@163.com>
2020-10-29 15:19:17 +0800

    merge fkbranch1 to master

--- eureka/src/test/java/com/fk/springboot/eureka/eureka/B.java
+++ eureka/src/test/java/com/fk/springboot/eureka/eureka/B.java
@@ -5,7 +5,7 @@ package com.fk.springboot.eureka.eureka;
  */
 public class B {
     public static void main(String[] args) {
-        System.out.println("这是在fkbranch1分支上做的修改");
+        System.out.println("这是在fkbranch1分支上做的修改 -- merge到master");
     }
 
 }
