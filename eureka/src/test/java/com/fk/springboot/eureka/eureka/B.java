0a62dea8ffa07113e0c39efef46b0b2c72a3644c
michale <lxfeng1163@163.com>
2020-10-29 15:11:09 +0800

    update B.java on fkbranch1

--- eureka/src/test/java/com/fk/springboot/eureka/eureka/B.java
+++ eureka/src/test/java/com/fk/springboot/eureka/eureka/B.java
@@ -4,4 +4,8 @@ package com.fk.springboot.eureka.eureka;
  * @author Michael.liu
  */
 public class B {
+    public static void main(String[] args) {
+        System.out.println("这是在fkbranch1分支上做的修改");
+    }
+
 }
